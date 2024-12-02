fcee05c8ea3b6faf9438c07e7ee1c106970b730b
lioncio320 <alitaszgodz@gmail.com>
2024-12-01 23:30:13 -0500

    grid

--- js/logic.js
+++ js/logic.js
@@ -3,5 +3,23 @@
 	padding: 0;
 	box-sizing: border-box;
 	font-family: sans-serif;
-	
+
+}
+body{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: 100vh;
+	background: #e3f2fd;
+}
+.wrapper{
+	width: 65vmin;
+	height: 70vmin;
+	background: #293447;
+}
+.game_details{
+	color: #b8c6dc;
+	font-size: 1.2rem;
+	font-weight: 500;
+	padding: 20px 27px;
 }
